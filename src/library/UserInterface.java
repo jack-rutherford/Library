0d06d9e7e2cbbb050c4b3e907c3a66cf3c6ec889
JustinFay01 <justin.fay@hope.edu>
2022-03-15 14:48:21 -0400

    GUI Skeleton & Main method

--- src/library/UserInterface.java
+++ src/library/UserInterface.java
@@ -1,11 +1,16 @@
 package library;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+
 import javax.swing.*;
 
-public class UserInterface implements IUserInterface {
+
+public class UserInterface extends JFrame {
 	
 	private JPanel homePanel;
-	private JButton displayCollection, Checkout, Quit;
+	private JButton displayCollection, checkout, quit;
 	private JLabel title;
 	private final int WINDOW_WIDTH = 1000, WINDOW_HEIGHT = 500;
 
@@ -13,32 +18,57 @@ public class UserInterface implements IUserInterface {
 	 * Initializes JPanel features. 
 	 */
 	public UserInterface() {
-		displayCollection = new JButton("Display Collection");
-		Checkout = new JButton("Checkout");
-		title = new JLabel("Library");
-		System.out.println("Working");
-		
-		//Register buttons with action listener. 
-		
+	
+		setTitle("Library");
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		buildPanel();
+		add(homePanel);
+		setVisible(true);
+
 		
 	}
 	
-	@Override
-	public void runProgram() {
+	
+	public void runProgram() throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		
 	}
-
-	@Override
-	public void buildPanel() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
+	
 	public void greeting() {
 		// TODO Auto-generated method stub
 		
 	}
 
+
+
+	public void buildPanel() {
+		//Builds GUI Panel 
+		displayCollection = new JButton("Display Collection");
+		checkout = new JButton("Checkout");
+		title = new JLabel("Library");
+		
+		//Register buttons with action listener
+		displayCollection.addActionListener(new ConvertButtonListener());
+		checkout.addActionListener(new ConvertButtonListener());
+		quit.addActionListener(new ConvertButtonListener());
+		
+	}
+
+	
+
+private class ConvertButtonListener implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// Makes buttons usable
+		
+		if(e.getSource() == displayCollection) {
+			//display collection...
+	}
+		else if(e.getSource() == checkout) {
+			//open checkout page...
+		}
+	}	
+	}
 }
