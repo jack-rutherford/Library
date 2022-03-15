79e48c712ff5d40e8e09e02a41a88688081f1753
JustinFay01 <justin.fay@hope.edu>
2022-03-15 14:09:38 -0400

    Userinterface Setup

--- src/library/IUserInterface.java
+++ src/library/IUserInterface.java
@@ -1,5 +1,17 @@
 package library;
 
 public interface IUserInterface {
-
+	/**
+	 * Runs the methods in the correct order for the program to execute correctly
+	 */
+	public void runProgram();
+	/**
+	 * Builds the panel for the GUI
+	 */
+	public void buildPanel();
+	/**
+	 * Displays basic greeting for the user
+	 */
+	public void greeting();
+	
 }
--- src/library/UserInterface.java
+++ src/library/UserInterface.java
@@ -2,4 +2,22 @@ package library;
 
 public class UserInterface implements IUserInterface {
 
+	@Override
+	public void runProgram() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void buildPanel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void greeting() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
