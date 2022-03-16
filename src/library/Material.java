ad5c1b72c36087f1e2840be22f9b303c3aa03aa3
Jack Rutherford <jack.rutherford@hope.edu>
2022-03-16 08:49:15 -0400

    Changed setDueDate method

--- src/library/Material.java
+++ src/library/Material.java
@@ -4,45 +4,50 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public abstract class Material {
-	
-//	private int checkOutTime;
+
+	//	private int checkOutTime;
 	private String title;
 	private String callNumber;
 	private boolean canBeCheckedOut;
 	private GregorianCalendar dateCheckedOut;
 	private Calendar dueDate;
-	
+
 	public Material(String title, String callNumber) {
 		this.title = title;
 		this.callNumber = callNumber;
 		canBeCheckedOut = true;
 		dateCheckedOut = new GregorianCalendar();
 		dueDate = (GregorianCalendar)dateCheckedOut.clone();
-		
+
 	}
-	
+
 	public boolean canBeCheckedOut() {
 		return canBeCheckedOut();
 	}
-	
+
 	public void setCanBeCheckedOut(boolean canBeCheckedOut) {
 		this.canBeCheckedOut = canBeCheckedOut;
 	}
-	
+
 	public String getTitle() {
 		return title;
 	}
-	
+
 	public String getCallNumber() {
 		return callNumber;
 	}
-	
-	public void setDueDate(int daysUntilDue) {
-		dueDate.add(Calendar.DAY_OF_YEAR, daysUntilDue);
+
+	public void setDueDate() {
+		if(this instanceof Book) {
+			dueDate.add(Calendar.DAY_OF_YEAR, 21);
+		}
+		else {
+			dueDate.add(Calendar.DAY_OF_YEAR, 7);
+		}
 	}
-	
+
 	public Calendar getDueDate() {
 		return dueDate;
 	} 
-	
+
 }
