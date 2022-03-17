7aca2799f85993a53b8d01147dd70d41c59dc2fa
Jack Rutherford <jack.rutherford@hope.edu>
2022-03-16 22:55:24 -0400

    Library test cases

--- src/tests/Library_Tests.java
+++ src/tests/Library_Tests.java
@@ -2,13 +2,22 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
+
+import library.Library;
+import library.Material;
 
 public class Library_Tests {
 
 	@Test
 	public void testLoadMaterials() {
-		fail("Not yet implemented");
+//		Library lib = new Library();
+//		ArrayList<Material> mats = lib.getMaterialList();
+//		int size = mats.size();
+//		assertEquals("there should be 4 materials in the mats arraylist", 4, size);
+
 	}
 
 	@Test
