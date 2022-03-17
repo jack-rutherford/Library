c8fb1e070291c0893493b76930b4b631679c8617
Jack Rutherford <jack.rutherford@hope.edu>
2022-03-16 22:53:05 -0400

    Github said this might fix the problem

---
+++ src/tests/Library_Tests.java
@@ -0,0 +1,24 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Library_Tests {
+
+	@Test
+	public void testLoadMaterials() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckOut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayCollection() {
+		fail("Not yet implemented");
+	}
+
+}
